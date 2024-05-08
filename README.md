e85ee42d659a21a22ea8b085b9e1927485413eb2
JamesMoriartyDecripto <james.moriarty@decripto.org>
2024-05-08 16:22:43 +0100

    messo il percorso dove andare in automatico

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # GitHubDesklet
 Simple Linux Debian desklet to track GitHub's notification
+
+## Manual installation
+
+1. clone this repo
+
+```
+git clone 
+```
+
+2. enter the directory
+
+```
+cd 
+```
+
+3. install the desklet
+
+```
+chmod +x install_desklet.sh
+```
+
+```
+./install_desklet.sh
+```
---
+++ install_desklet.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Definisce il percorso della cartella sorgente del desklet
+SOURCE_DIR="$(pwd)/github-notifier@JamesMoriartyDecripto"
+
+# Definisce il percorso della cartella di destinazione dei desklets
+DEST_DIR="/home/james/.local/share/cinnamon/desklets"
+
+# Nome del desklet
+DESKLET_DIR="github-notifier@JamesMoriartyDecripto"
+
+# Controlla se la cartella esiste già nella destinazione
+if [ -d "$DEST_DIR/$DESKLET_DIR" ]; then
+    echo "Rimozione della vecchia versione del desklet..."
+    rm -rf "$DEST_DIR/$DESKLET_DIR"
+fi
+
+# Copia la nuova versione del desklet nella directory dei desklets
+echo "Copia della nuova versione del desklet..."
+cp -r "$SOURCE_DIR" "$DEST_DIR"
+
+# Conferma operazione completata
+echo "Desklet installato correttamente in $DEST_DIR/$DESKLET_DIR"
